fb80ac34d118c0468681a57ef2c7489349339655
jennyg0 <jen2348@hotmail.com>
2025-01-17 15:36:45 -0500

    add access control

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -2,93 +2,98 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract RebaseToken is ERC20 {
-  error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
+contract RebaseToken is ERC20, Ownable, AccessControl {
+    error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
 
-  uint256 private constant PRECISION_FACTOR = 1e18;
-  uint256 private s_interestRate = 5e10;
-  mapping(address => uint256) private s_userInterestRate;
-  mapping(address =>uint256) private s_userLastUpdatedTimestamp;
+    uint256 private constant PRECISION_FACTOR = 1e18;
+    bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
+    uint256 private s_interestRate = 5e10;
+    mapping(address => uint256) private s_userInterestRate;
+    mapping(address => uint256) private s_userLastUpdatedTimestamp;
 
-  event InterestRateChanged(uint256 newInterestRate);
+    event InterestRateChanged(uint256 newInterestRate);
 
-  constructor() ERC20("RebaseToken", "RBT") {
-    
-  }
-  
-  function setInterestRate(uint256 _interestRate) external {
-    if (_interestRate > s_interestRate) {
-      revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _interestRate);
+    constructor() ERC20("RebaseToken", "RBT") Ownable(msg.sender) {}
+
+    function grantMintAndBurnRole(address _account) external onlyOwner {
+        _grantRole(MINT_AND_BURN_ROLE, _account);
     }
-    s_interestRate = _interestRate;
-    emit InterestRateChanged(_interestRate);
-  }
 
-  function principleBalance(address _user) external view returns (uint256) {
-    return super.balanceOf(_user);
-  }
-
-  function mint(address _to, uint256 _amount) external {
-    _mintAccruedInterest(_to);
-    s_interestRate[_to] = s_interestRate;
-    _mint(_to, _amount);
-  } 
-
-  function balanceOf(address _user) public view override returns (uint256) {
-    return super.balanceOf(_user) * _getAccruedInterest(_user) / PRECISION_FACTOR;
-  }
-
-  function transfer(address _recipient, uint256 _amount) public override returns (bool) {
-    _mintAccruedInterest(msg.sender);
-    _mintAccruedInterest(_recipient);
-    if(_amount == type(uint256).max) {
-      _amount = balanceOf(msg.sender);
+    function setInterestRate(uint256 _interestRate) external onlyOwner {
+        if (_interestRate > s_interestRate) {
+            revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _interestRate);
+        }
+        s_interestRate = _interestRate;
+        emit InterestRateChanged(_interestRate);
     }
-    if(balanceOf(_recipient) == 0) {
-      s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
+
+    function principleBalance(address _user) external view returns (uint256) {
+        return super.balanceOf(_user);
     }
-    return super.transfer(_recipient, _amount);
-  }
 
-  function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
-    _mintAccruedInterest(_sender);
-    _mintAccruedInterest(_recipient);
-    if(_amount == type(uint256).max) {
-      _amount = balanceOf(_sender);
+    function mint(address _to, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
+        _mintAccruedInterest(_to);
+        s_userInterestRate[_to] = s_interestRate;
+        _mint(_to, _amount);
     }
-    if(balanceOf(_recipient) == 0) {
-      s_userInterestRate[_recipient] = s_userInterestRate[_sender];
+
+    function balanceOf(address _user) public view override returns (uint256) {
+        return super.balanceOf(_user) * _getAccruedInterest(_user) / PRECISION_FACTOR;
     }
-    return super.transferFrom(_sender, _recipient, _amount);
-  }
 
-  function _getAccruedInterest(address _user) internal view returns (uint256) {
-    uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
-   linearInterest = PRECISION_FACTOR + (s_userInterestRate[_user] * timeElapsed);
-  }
-
-  function _mintAccruedInterest(address _user) internal {
-    uint256 previousPrincipleBalance = super.balanceOf(_user);
-    uint256 currentBalance = balanceOf(_user);
-    uint256 balanceIncrease = currentBalance - previousPrincipleBalance;
-    s_userLastUpdatedTimestamp[_user] = block.timestamp;
-    _mint(_user, balanceIncrease);
-  }
-
-  function burn(address _from, uint256 _amount) external {
-    if (_amount == type.(uint256).max) {
-      _amount = balanceOf(_from);
+    function transfer(address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruedInterest(msg.sender);
+        _mintAccruedInterest(_recipient);
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(msg.sender);
+        }
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
+        }
+        return super.transfer(_recipient, _amount);
     }
-    _mintAccruedInterest(_from);
-    _burn(_from, _amount);
-  }
 
-  function getUserInterestRate(address _user) external view returns (uint256) {
-    return s_userInterestRate[_user];
-  }
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruedInterest(_sender);
+        _mintAccruedInterest(_recipient);
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_sender);
+        }
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[_sender];
+        }
+        return super.transferFrom(_sender, _recipient, _amount);
+    }
 
-  function getInterestRate() external view returns (uint256) {
-    return s_interestRate;
-  }
+    function _getAccruedInterest(address _user) internal view returns (uint256 linearInterest) {
+        uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
+        linearInterest = PRECISION_FACTOR + (s_userInterestRate[_user] * timeElapsed);
+    }
+
+    function _mintAccruedInterest(address _user) internal {
+        uint256 previousPrincipleBalance = super.balanceOf(_user);
+        uint256 currentBalance = balanceOf(_user);
+        uint256 balanceIncrease = currentBalance - previousPrincipleBalance;
+        s_userLastUpdatedTimestamp[_user] = block.timestamp;
+        _mint(_user, balanceIncrease);
+    }
+
+    function burn(address _from, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_from);
+        }
+        _mintAccruedInterest(_from);
+        _burn(_from, _amount);
+    }
+
+    function getUserInterestRate(address _user) external view returns (uint256) {
+        return s_userInterestRate[_user];
+    }
+
+    function getInterestRate() external view returns (uint256) {
+        return s_interestRate;
+    }
 }
